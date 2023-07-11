9fbd6bbcf33b215cf9393cdba325c6b82f8df064
Tony-ArtZ <adarshgku@gmail.com>
2023-07-11 12:23:10 +0530

    added page effect to text area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="bg-blue-dark">
+    <main className="">
      <div>
-      <h1 className='text-xl text-green'>Hello</h1>
+       <div className="before:content-[_'] before:absolute before:h-screen before:left-5 before:w-[1px] before:bg-yellow before:z-20">
+        <textarea className="w-screen h-screen leading-[30px] text-lg relative px-8 text-white bg-paper"/>
+       </div>
      </div>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,14 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'blue-dark': '#205072',
+        'blue-dark': '#12182B',
+        'blue': '#394F6B',
         'green-dark': '#329D9C',
         'green': '#7BE495',
         'yellow': '#E5EFC1',
+      },
+      backgroundImage: {
+        "paper": "repeating-linear-gradient(#12182B 0px, #12182B 29px, #7BE495 30px)",
       }
     },
   },
